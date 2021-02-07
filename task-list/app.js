a56c7ad986269ff439ef9630c50dd14163488b50
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-07 22:15:08 +0100

    ✨ Store tasks in local storage

--- task-list/app.js
+++ task-list/app.js
@@ -9,13 +9,37 @@ const clearBtn = document.querySelector(".clear-tasks-btn");
 loadEventListeners();
 
 function loadEventListeners() {
+  document.addEventListener("DOMContentLoaded", getTasks);
   form.addEventListener("submit", addTask);
   taskList.addEventListener("click", deleteTask);
   clearBtn.addEventListener("click", deleteAllTasks);
   filter.addEventListener("keyup", filterTasks);
 }
 
-// Add task
+// get tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    // create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // create new link element
+    const link = document.createElement("span");
+    link.className = "delete-item";
+    link.innerHTML =
+      '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>';
+    li.appendChild(link);
+    //append li to ul
+    taskList.appendChild(li);
+  });
+}
+
 function addTask(e) {
   e.preventDefault();
   if (taskInput.value === "") {
@@ -34,19 +58,45 @@ function addTask(e) {
     li.appendChild(link);
     //append li to ul
     taskList.appendChild(li);
+    // Store to Local Storage(LS)
+    storeTaskInLocalStorage(taskInput.value);
     // clear input
     taskInput.value = "";
-
-    taskList.appendChild(li);
   }
+}
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function deleteTask(e) {
   if (e.target.closest(".delete-item")) {
     if (confirm("Are You Sure?")) {
       e.target.closest("li").remove();
+      removeTaskFromLocalStorage(e.target.closest("li"));
     }
   }
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function deleteAllTasks(e) {
@@ -55,6 +105,7 @@ function deleteAllTasks(e) {
     while (taskList.firstChild) {
       taskList.removeChild(taskList.firstChild);
     }
+    localStorage.clear();
   }
 }
 
