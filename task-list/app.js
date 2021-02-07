49782305e96dbec9f9b92c3214cc5028eb36f177
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-07 11:41:40 +0100

    📔 Define UI vars

---
+++ task-list/app.js
@@ -0,0 +1,6 @@
+// Define UI vars
+const form = document.querySelector(".add-card");
+const taskInput = document.querySelector("#task");
+const taskList = document.querySelector(".collection");
+const filter = document.querySelector("#filter");
+const clearBtn = document.querySelector(".clear-tasks-btn");
