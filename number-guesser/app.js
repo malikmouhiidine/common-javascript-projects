b163bfe143bdfae270b36e39afd53ae3c38b7a0b
Malik Mouhiidine <malikprofessional2020@gmail.com>
2021-02-08 18:47:14 +0100

    ✨ play again

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -10,6 +10,12 @@ const game = document.getElementById("game"),
   guessBtn = document.getElementById("guess-button"),
   guessInput = document.getElementById("guess-input"),
   message = document.getElementById("message");
+// Play again event listener
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
 
 minNum.textContent = min;
 maxNum.textContent = max;
@@ -17,14 +23,33 @@ guessBtn.addEventListener("click", function () {
   let guess = parseInt(guessInput.value);
   if (isNaN(guess) || guess > max || guess < min) {
     setMessage(`Please enter a number between ${min} and ${max}`, "red");
+    return;
   }
   if (guess === winningNum) {
-    guessInput.disabled = true;
-    guessInput.style.borderColor = "green";
-    setMessage(`Yes ${guess} is the correct number`, "green");
+    setMessage(`Yes ${guess} is the correct number`, "green", true);
+  } else {
+    // Wrong number
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      setMessage(
+        `${guessesLeft} guesses left the correct number was ${winningNum}`,
+        "red",
+        true
+      );
+    } else {
+      setMessage(`Not Correct. ${guessesLeft} guesses left`, "red");
+    }
   }
+  guessInput.value = "";
 });
-function setMessage(msg, color) {
+
+function setMessage(msg, color, gameOver = false) {
+  if (gameOver === true) {
+    guessInput.disabled = true;
+    guessBtn.value = "Play Again";
+    guessBtn.className += "play-again";
+  }
+  guessInput.style.borderColor = color;
   message.style.color = color;
   message.textContent = msg;
 }
